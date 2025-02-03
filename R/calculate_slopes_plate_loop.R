60244e94eae62663cac24d7dba58312c48f4f78b
dcowanturner <dannycowanturner@gmail.com>
2025-02-03 16:51:44 +1100

    Fixed well matching bug

--- R/calculate_slopes_plate_loop.R
+++ R/calculate_slopes_plate_loop.R
@@ -1,8 +1,8 @@
 #' calculate_slopes_plate_loop
 #'
-#' New version that calculate_slopes_for_periods
+#' Fixed  030225 - Issue with well matching
 #'
-#' @param raw.data.clean,metadata,time_windows Define overlapping time windows
+#' @param raw.data.clean,metadata,time_windows TEST
 #' @return output_plate_data
 #' @export
 
@@ -14,9 +14,15 @@ calculate_slopes_plate_loop <- function (Input.plate.data, Input.plate.metadata,
     raw_data <- as.data.frame(Input.plate.data[[i]])
     raw_data_clean <- Q2_Clean(raw_data)
     metadata <- as.data.frame(Input.plate.metadata[[i]])
-    metadata <- metadata %>% select(-Well)
-    output <- cbind(calculate_slopes_for_periods(raw_data_clean,
-                                                 metadata,time_windows), metadata)
+    # metadata <- metadata %>% mutate(metaWell=Well)  %>% select(-Well)
+
+
+    output <- calculate_slopes_for_periods(raw_data_clean,
+                                           metadata,time_windows)
+
+
+    output <- left_join(metadata,Testout,by="Well")
+
     output_plate_data[[i]] <- output
   }
   return(output_plate_data)
